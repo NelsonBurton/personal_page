dfb8be0d60986cb3936f873c96ab1a6da7a22807
Nelson Burton <nelsonb@gmail.com>
2015-05-20 18:43:02 -0400

    finish btc article

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'  
-  get 'downloadresume' => 'static_pages#downloadresume'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
