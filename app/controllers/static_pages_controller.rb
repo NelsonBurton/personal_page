20ad759e0215ecf139ee2dbd0027924f738de2ce
Nelson Burton <nelsonb@gmail.com>
2015-05-20 16:07:56 -0400

    todd changes to design

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,4 @@ class StaticPagesController < ApplicationController
 	end
 	def about
 	end
-	def downloadresume
-   		send_file 'public/data/BurtonResume2015.pdf', :type=>"application/pdf", :x_sendfile=>true
-	end
 end
